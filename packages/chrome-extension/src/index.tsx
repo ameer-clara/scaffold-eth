b4f9a4d7a06fd745ee8b0df95be1171cd143c0aa
Ameer Ahmed <ameer@clara.doctor>
2022-11-27 14:16:47 -0500

    Simple wallet provider to connect to metamask

--- packages/chrome-extension/src/index.tsx
+++ packages/chrome-extension/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import WalletProvider from './context/WalletProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WalletProvider>
+      <App />
+    </WalletProvider>
   </React.StrictMode>,
   document.getElementById('root') as HTMLElement
 );
